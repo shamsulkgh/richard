38d579d4a0bf2de9a2e61f4411d875b209cad128
shamsulkgh <100175768+shamsulkgh@users.noreply.github.com>
2022-02-28 18:59:39 +0800

    Update fetch-weather.js

--- functions/fetch-weather/fetch-weather.js
+++ functions/fetch-weather/fetch-weather.js
@@ -1,9 +1,11 @@
-const axios = require("axios");
+const axios = require('axios')
+const qs = require('qs')
 
 const handler = async (event) => {
+  const API_PARAMS = qs.stringify(event.queryStringParameters)
   const {maxRecords, view} = event.queryStringParameters
   try {
-    const { data } = await axios.get("https://api.airtable.com/v0/appH9pGf1FeVfhzUe/Table1", {
+    const { data } = await axios.get("https://api.airtable.com/v0/appH9pGf1FeVfhzUe/Table1?${API_PARAMS}", {
       headers: {
         authorization: process.env.API_SECRET,
       },
